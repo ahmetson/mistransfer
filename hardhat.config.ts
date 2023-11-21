15265bad6f7a4c420ab21b519c8a74f13e97ebed
Medet Ahmetson <ahmetson@zoho.com>
2023-11-21 10:38:20 +0700

    Add etherscan verifier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,12 +3,16 @@ dotenv.config(); // call it before initializing `vars`
 
 import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 
 
 const INFURA_API_KEY = process.env.HARDHAT_VAR_INFURA_API_KEY as string;
 const SEPOLIA_PRIVATE_KEY = process.env.HARDHAT_VAR_SEPOLIA_PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
+  etherscan: {
+    apiKey: process.env.HARDHAT_VAR_ETHERSCAN as string,
+  },
   solidity: "0.8.22",
   networks: {
     sepolia: {
