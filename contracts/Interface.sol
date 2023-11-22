56264b69276b4620f3ce6553b380ada3064dc4e1
Medet Ahmetson <ahmetson@zoho.com>
2023-11-22 21:07:35 +0700

    Upgrade the user interface

--- contracts/Interface.sol
+++ contracts/Interface.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.2;
 
 interface Interface {
-    function recoverMyNft(bytes calldata txHash, address targetContract, address token, uint tokenId) payable external;
-    function recoverMyToken(bytes calldata txHash, address targetContract, address token, uint amount) payable external;
+    function recoverMyNft(bytes32 txHash, address targetContract, address token, uint tokenId) payable external;
+    function recoverMyToken(bytes32 txHash, address targetContract, address token, uint amount) payable external;
     function removeUrl(string calldata url) external;
     function addUrl(string calldata url) external;
 }
--- contracts/UserInterface.sol
+++ contracts/UserInterface.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.2;
 
 import "./Interface.sol";
 import "./UserCaringInterface.sol";
-import "./Sources.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
@@ -20,6 +19,7 @@ contract UserInterface is Interface, FunctionsClient {
         address targetContract;
         address token;
         uint tokenId;
+        bytes32 txHash;
     }
 
     struct TokenParams {
@@ -27,8 +27,10 @@ contract UserInterface is Interface, FunctionsClient {
         address targetContract;
         address token;
         uint amount;
+        bytes32 txHash;
     }
 
+    address public owner;
     address public chainLinkRouter;
     uint64 public chainLinkSubId;
     bytes32 public chainLinkDonId;
@@ -43,12 +45,17 @@ contract UserInterface is Interface, FunctionsClient {
     mapping(address => bytes32) public requests;
     mapping(bytes32 => NftParams) public recoverNftParams;
     mapping(bytes32 => TokenParams) public recoverTokenParams;
+    mapping(bytes32 => mapping(address => bool)) public recoveredNfts;
+    mapping(bytes32 => mapping(address => bool)) public recoveredTokens;
+
+    string public tokenSource = "";
+    string public nftSource = "";
 
     error UnexpectedRequestID(bytes32 requestId);
 
     event Response(address indexed caller, bytes32 indexed requestId, bytes response, bytes err);
 
-    constructor(address _router, uint64 _subId, bytes32 _donId, string memory _url) FunctionsClient(_router) {
+    constructor(address _router, uint64 _subId, bytes32 _donId) FunctionsClient(_router) {
         // for sepolia 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0. See the
         // https://docs.chain.link/chainlink-functions/supported-networks
         chainLinkRouter = _router;
@@ -59,15 +66,18 @@ contract UserInterface is Interface, FunctionsClient {
         // For sepolia: 0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000
         chainLinkDonId = _donId;
 
+        owner = msg.sender;
         fee = 10000000000000000;
+    }
 
-        addUrl(_url);
+    function setTokenSource(string calldata _source) external {
+        tokenSource = _source;
     }
 
     // https://www.appsloveworld.com/ethereum/32/convert-bytes-to-hexadecimal-string-in-solidity
-    function iToHex(bytes memory buffer) public pure returns (string memory) {
+    function iToHex(bytes32 buffer) public pure returns (string memory) {
         // Fixed buffer size for hexadecimal convertion
-        bytes memory converted = new bytes(buffer.length * 2);
+        bytes memory converted = new bytes(64 * 2);
 
         bytes memory _base = "0123456789abcdef";
 
@@ -79,24 +89,23 @@ contract UserInterface is Interface, FunctionsClient {
         return string(abi.encodePacked("0x", converted));
     }
 
-    // Todo make sure that token id is owned by targetContract
-    function recoverMyNft(bytes calldata txHash, address targetContract, address token, uint tokenId) payable external {
+    function recoverMyNft(bytes32 txHash, address targetContract, address token, uint tokenId) payable external {
         IERC721 nft = IERC721(token);
         require(nft.ownerOf(tokenId) == targetContract, "not owner");
 
         require(msg.value >= fee, "not_enough_fee");
         require(targetContract.code.length > 0, "target not deployed");
         require(requests[msg.sender] == 0, "request pending");
+        require(!recoveredNfts[txHash][msg.sender], "already_claimed");
 
         FunctionsRequest.Request memory req;
-        req.initializeRequestForInlineJavaScript(Sources.nftSource); // Initialize the request with JS code
-        string[] memory args = new string[](6);
+        req.initializeRequestForInlineJavaScript(nftSource); // Initialize the request with JS code
+        string[] memory args = new string[](5);
         args[0] = iToHex(txHash); // transaction hash
         args[1] = Strings.toHexString(msg.sender); // user
         args[2] = Strings.toHexString(targetContract); // contract
         args[3] = Strings.toHexString(token); // token
         args[4] = Strings.toString(tokenId); // token id
-        args[5] = url; // rpc url;
         req.setArgs(args); // Set the arguments for the request
 
         // Send the request and store the request ID
@@ -107,14 +116,41 @@ contract UserInterface is Interface, FunctionsClient {
             chainLinkDonId
         );
 
-        recoverNftParams[requests[msg.sender]] = NftParams(msg.sender, targetContract, token, tokenId);
+        recoverNftParams[requests[msg.sender]] = NftParams(msg.sender, targetContract, token, tokenId, txHash);
     }
 
-    function fulfillRequest(
-        bytes32 requestId,
-        bytes memory response,
-        bytes memory err
-    ) internal override {
+    function recoverMyToken(bytes32 txHash, address targetContract, address token, uint amount) payable external {
+        IERC20 erc20 = IERC20(token);
+        require(erc20.balanceOf(targetContract) >= amount, "not_enough");
+
+        require(msg.value >= fee, "not_enough_fee");
+
+        require(targetContract.code.length > 0, "target not deployed");
+        require(requests[msg.sender] == 0, "request pending");
+        require(!recoveredTokens[txHash][msg.sender], "already_claimed");
+
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(tokenSource); // Initialize the request with JS code
+        string[] memory args = new string[](5);
+        args[0] = iToHex(txHash); // transaction hash
+        args[1] = Strings.toHexString(msg.sender); // user
+        args[2] = Strings.toHexString(targetContract); // contract
+        args[3] = Strings.toHexString(token); // token
+        args[4] = Strings.toString(amount); // token id
+        req.setArgs(args); // Set the arguments for the request
+
+        // Send the request and store the request ID
+        requests[msg.sender] = _sendRequest(
+            req.encodeCBOR(),
+            chainLinkSubId,
+            gasLimit,
+            chainLinkDonId
+        );
+
+        recoverTokenParams[requests[msg.sender]] = TokenParams(msg.sender, targetContract, token, amount, txHash);
+    }
+
+    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
         if (recoverNftParams[requestId].caller == address(0) &&
             recoverTokenParams[requestId].caller == address(0)) {
             revert UnexpectedRequestID(requestId); // Check if request IDs match
@@ -141,9 +177,13 @@ contract UserInterface is Interface, FunctionsClient {
         UserCaringInterface userCaring = UserCaringInterface(targetContract);
         if (nft) {
             userCaring.recoverUserNft(token, caller, recoverNftParams[requestId].tokenId);
+            recoveredNfts[recoverNftParams[requestId].txHash][recoverNftParams[requestId].caller] = true;
+
             delete recoverNftParams[requestId];
         } else {
             userCaring.recoverUserToken(token, caller, recoverTokenParams[requestId].amount);
+            recoveredTokens[recoverTokenParams[requestId].txHash][recoverTokenParams[requestId].caller] = true;
+
             delete recoverTokenParams[requestId];
         }
 
@@ -152,45 +192,18 @@ contract UserInterface is Interface, FunctionsClient {
         delete requests[caller];
     }
 
-    // Todo make sure that target contract has enough tokens
-    function recoverMyToken(bytes calldata txHash, address targetContract, address token, uint amount) payable external {
-        IERC20 erc20 = IERC20(token);
-        require(erc20.balanceOf(targetContract) > amount, "not_enough");
-
-        require(msg.value >= fee, "not_enough_fee");
-
-        require(targetContract.code.length > 0, "target not deployed");
-        require(requests[msg.sender] == 0, "request pending");
-
-        FunctionsRequest.Request memory req;
-        req.initializeRequestForInlineJavaScript(Sources.tokenSource); // Initialize the request with JS code
-        string[] memory args = new string[](6);
-        args[0] = iToHex(txHash); // transaction hash
-        args[1] = Strings.toHexString(msg.sender); // user
-        args[2] = Strings.toHexString(targetContract); // contract
-        args[3] = Strings.toHexString(token); // token
-        args[4] = Strings.toString(amount); // token id
-        args[5] = url; // rpc url;
-        req.setArgs(args); // Set the arguments for the request
-
-        // Send the request and store the request ID
-        requests[msg.sender] = _sendRequest(
-            req.encodeCBOR(),
-            chainLinkSubId,
-            gasLimit,
-            chainLinkDonId
-        );
-
-        recoverTokenParams[requests[msg.sender]] = TokenParams(msg.sender, targetContract, token, amount);
-    }
-
     // Todo Make add access control
-    function removeUrl(string calldata _url) public {
-
+    function removeUrl(string calldata) public {
+        url = "";
     }
 
     // Todo make add access control
     function addUrl(string memory _url) public {
         url = _url;
+    }
+
+    // for test only
+    function withdraw() external {
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
